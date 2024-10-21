52aff4aa50ae5432e1f7695b111cf63ef66705f0
Jordan <developer@cycurid.com>
2024-10-21 15:57:40 -0700

    fixed upload portable did

--- test-dids/test.mjs
+++ test-dids/test.mjs
@@ -10,11 +10,12 @@ const instances = {};
 const didLabelRegex = /(?:[^@]*@)?(did:[a-z0-9]+:[a-zA-Z0-9-]+)/;
 
 const storageFilePath = './storage.json';
+const storageIdentityFilePath = './storageIdentity.json';
 
 const storage = {
   get: (key, _default) => {
     const data = JSON.parse(fs.readFileSync(storageFilePath, 'utf-8') || '{}');
-    let value = data['web5:' + key];
+    let value = data[key];
     if (value) return value;
     if (_default !== undefined) {
       storage.set(key, _default);
@@ -23,14 +24,44 @@ const storage = {
   },
   set: (key, value) => {
     let data = JSON.parse(fs.readFileSync(storageFilePath, 'utf-8') || '{}');
-    data['web5:' + key] = value;
+    data[key] = value;
     fs.writeFileSync(storageFilePath, JSON.stringify(data, null, 2), 'utf-8');
     return value;
   },
   modify: (key, fn) => {
     const value = storage.get(key);
     return storage.set(key, fn(value));
-  }
+  },
+};
+
+const storageIdentity = {
+  get: (key, _default) => {
+    const data = JSON.parse(
+      fs.readFileSync(storageIdentityFilePath, 'utf-8') || '{}'
+    );
+    let value = data[key];
+    if (value) return value;
+    if (_default !== undefined) {
+      storageIdentity.set(key, _default);
+      return _default;
+    }
+  },
+  set: (key, value) => {
+    let data = JSON.parse(
+      fs.readFileSync(storageIdentityFilePath, 'utf-8') || '{}'
+    );
+    data[key] = value;
+    fs.writeFileSync(
+      storageIdentityFilePath,
+      JSON.stringify(data, null, 2),
+      'utf-8'
+    );
+    return value;
+  },
+  modify: (key, fn) => {
+    const value = storageIdentity.get(key);
+    return storageIdentity.set(key, fn(value));
+  },
 };
 
 function getUserDidOptions(endpoints) {
@@ -42,24 +73,29 @@ function getUserDidOptions(endpoints) {
         serviceEndpoint: ['http://localhost:3000'],
         enc: '#enc',
         sig: '#sig',
-      }
+      },
     ],
     verificationMethods: [
       {
         algorithm: 'Ed25519',
         id: 'sig',
-        purposes: ['assertionMethod', 'authentication']
+        purposes: ['assertionMethod', 'authentication'],
       },
       {
         algorithm: 'secp256k1',
         id: 'enc',
-        purposes: ['keyAgreement']
-      }
-    ]
+        purposes: ['keyAgreement'],
+      },
+    ],
   };
 }
 
-async function registerEndpoints(agent, identity, dwnEndpoints = [], registration) {
+async function registerEndpoints(
+  agent,
+  identity,
+  dwnEndpoints = [],
+  registration
+) {
   try {
     for (const endpoint of dwnEndpoints) {
       const serverInfo = await agent.rpc.getServerInfo(endpoint);
@@ -88,46 +124,53 @@ async function getPortableDid(identity) {
 async function importIdentity(agent, portableIdentity, manage = false) {
   const uri = portableIdentity?.portableDid?.uri;
   let exists = await agent.identity.get({ didUri: uri });
+  console.log('exists=>', exists);
   if (!exists) {
     let identity = await agent.identity.import({ portableIdentity });
-    if (manage) {
-      return await agent.identity.manage({ portableIdentity });
-    }
     return identity;
   }
+
+  console.log('PORTABLE IDENTITY ALREADY EXISTS');
+  return { exists, sync: false };
 }
 
 export const DWeb = {
   storage,
+  storageIdentity,
   async initialize(options = {}) {
-    return initialize || (initialize = new Promise(async (resolve) => {
-      let did = storage.get('agentDid');
-      if (!did) {
-        if (options.portableAgent !== false) {
-          did = await DidDht.create({
-            options: getUserDidOptions(options.dwnEndpoints)
-          });
-        } else {
-          did = await DidJwk.create({
-            options: {
-              algorithm: 'Ed25519'
-            }
-          });
+    return (
+      initialize ||
+      (initialize = new Promise(async (resolve) => {
+        let did = storage.get('agentDid');
+        if (!did) {
+          if (options.portableAgent !== false) {
+            did = await DidDht.create({
+              options: getUserDidOptions(options.dwnEndpoints),
+            });
+          } else {
+            did = await DidJwk.create({
+              options: {
+                algorithm: 'Ed25519',
+              },
+            });
+          }
+          did = await did.export();
+          console.log('did==>', did);
+          storage.set('agentDid', did);
         }
-        did = await did.export();
-        storage.set('agentDid', did);
-      }
-      let agentDid = await BearerDid.import({ portableDid: did });
-      const agent = DWeb.agent = await Web5UserAgent.create({ agentDid });
-      agent.sync.startSync({ interval: '5s' }).catch((error) => {
-        console.error(`Sync failed: ${error}`);
-      });
-      resolve(DWeb.agent);
-    }));
+        let agentDid = await BearerDid.import({ portableDid: did });
+        const agent = (DWeb.agent = await Web5UserAgent.create({ agentDid }));
+        agent.sync.startSync({ interval: '5s' }).catch((error) => {
+          console.error(`Sync failed: ${error}`);
+        });
+        resolve(DWeb.agent);
+      }))
+    );
   },
   did: {
     async update(identity, modifier) {
-      if (!modifier) throw 'You must pass in a function that modifies a copy of the DID Document, or an already modified DID Document.';
+      if (!modifier)
+        throw 'You must pass in a function that modifies a copy of the DID Document, or an already modified DID Document.';
       if (typeof identity === 'string') {
         const agent = await getAgent();
         identity = await agent.identity.get({ didUri: identity });
@@ -146,7 +189,7 @@ export const DWeb = {
         identity.did.document = currentDoc;
         throw 'Failed to update DID Document';
       }
-    }
+    },
   },
   identity: {
     async create(options = {}) {
@@ -155,19 +198,26 @@ export const DWeb = {
       const identity = await agent.identity.create({
         didMethod: options.method || 'dht',
         metadata: { name: 'Default' },
-        didOptions: getUserDidOptions(dwnEndpoints)
+        didOptions: getUserDidOptions(dwnEndpoints),
       });
       if (options.cache !== false) {
-        await agent.identity.manage({ portableIdentity: await identity.export() });
+        await agent.identity.manage({
+          portableIdentity: await identity.export(),
+        });
       }
-      if (options.register!==false && dwnEndpoints) {
+      if (options.register !== false && dwnEndpoints) {
         const registration = {
-            onSuccess: () => console.log('DID successfully registered with DWN.'),
-            onFailure: (error) => console.error('Failed to register DID with DWN:', error)
-          };
-        await registerEndpoints(agent, identity, dwnEndpoints, registration)
-          .catch((e) => console.log('Endpoint registration failed: ', e));
-          console.log("registerEndpoints Successful: ", dwnEndpoints);
+          onSuccess: () => console.log('DID successfully registered with DWN.'),
+          onFailure: (error) =>
+            console.error('Failed to register DID with DWN:', error),
+        };
+        await registerEndpoints(
+          agent,
+          identity,
+          dwnEndpoints,
+          registration
+        ).catch((e) => console.log('Endpoint registration failed: ', e));
+        console.log('registerEndpoints Successful: ', dwnEndpoints);
       }
       return identity;
     },
@@ -177,21 +227,35 @@ export const DWeb = {
     },
     async get(uri) {
       const identities = await this.list();
-      return identities.find(identity => identity.did.uri === uri);
-    }
+      return identities.find((identity) => identity.did.uri === uri);
+    },
+    async backup(identity) {
+      const portableDid = await getPortableDid(identity);
+      storageIdentity.set('identity', portableDid);
+    },
   },
+
   async use(identity, options = {}) {
-    const uri = identity?.metadata?.uri || identity?.uri || identity;
+    console.log('identity: ==>', identity);
+    const uri =
+      identity?.metadata?.connectedDid ||
+      identity?.did?.uri ||
+      identity?.uri ||
+      identity;
+    console.log('uri==>', uri);
     let instance = instances[uri];
     if (instance) return instance;
     const agent = await getAgent();
     const entry = await DWeb.identity.get(uri);
+    console.log('entry==>', entry);
     if (!entry) {
-      await agent.identity.manage({ portableIdentity: await getPortableDid(identity) });
+      await agent.identity.manage({
+        portableIdentity: await getPortableDid(identity),
+      });
     }
     instance = new Web5({
       agent: agent,
-      connectedDid: uri
+      connectedDid: uri,
     });
     instances[uri] = instance;
     if (options.sync !== false) {
@@ -202,99 +266,100 @@ export const DWeb = {
   dispose(instance) {
     (instance?.agent || instance).sync.stopSync();
     delete instances[instance.connectedDid];
-  }
+  },
 };
 
-
-(async () => {
-    const agent = await DWeb.initialize();
-    console.log('Web5 agent initialized:', agent);
-  
-    const newIdentity = await DWeb.identity.create();
-    console.log('Created new identity:', newIdentity);
-  
-    const identity = await DWeb.identity.get(newIdentity.did.uri);
-    console.log('Fetched identity:', identity);
-
-    const web5Instance = await DWeb.use(identity);
-
-    const { record } = await web5Instance.dwn.records.create({
-        data: {
-          content: 'Hello Web5',
-          description: 'Keep Building!',
-        },
-        message: { published: true, dataFormat: 'application/json' },
-      });
-      
-      const response = await web5Instance.dwn.records.query({
-        message: {
-          filter: {
-            dataFormat: 'application/json',
-          },
-        },
-      });
-      
-      if (response.status.code === 200) {
-        response.records.forEach(async (record) => {
-          console.log(`recordID: ${record._recordId}=> `, await record.data.text());
-        });
-    }
-  })();
-
-
-  ///NOT WORKING SYNC MOT HAPENING???????
 // (async () => {
-//     // Initialize the Web5 agent
-//     const agent = await DWeb.initialize();
-//     agent.sync.startSync({ interval: '5s' }).catch((error) => {
-//         console.error(`Sync failed: ${error}`);
-//       });
-  
-//     // Check if an identity exists in storage
-//     let storedIdentity = DWeb.storage.get('identity');
-//     let newIdentity;
-  
-//     if (storedIdentity) {
-//       console.log('Found stored identity:');
-//       // Import the stored identity
-//       newIdentity = await DWeb.identity.get(storedIdentity.didUri);
-//     } else {
-//       // Create a new identity if none exists in storage
-//       newIdentity = await DWeb.identity.create();
-//       console.log('Created new identity:');
-//       // Store the new identity in storage for future use
-//       DWeb.storage.set('identity', { didUri: newIdentity.did.uri });
-//     }
-  
-//     console.log('Using identity:');
-  
-//     // Use the identity with the Web5 instance
-//     const web5Instance = await DWeb.use(newIdentity,{sync:false});
-  
-//     // Create a new record on the Decentralized Web Node (DWN)
-//     const { record } = await web5Instance.dwn.records.create({
-//       data: {
-//         content: 'Hello Web5',
-//         description: 'Keep Building!',
-//       },
-//       message: { published: true, dataFormat: 'application/json' },
-//     });
-  
-//     // Query records from the DWN
-//     const response = await web5Instance.dwn.records.query({
-//       message: {
-//         filter: {
-//           dataFormat: 'application/json',
-//         },
-//       },
-//     });
-  
-//     // If the query was successful, print out the records
-//     if (response.status.code === 200) {
-//       response.records.forEach(async (record) => {
-//         console.log(`recordID: ${record._recordId} =>`, await record.data.text());
-//       });
-//     }
-//   })();
-  
+//   const agent = await DWeb.initialize();
+//   console.log('Web5 agent initialized:', agent);
 
+//   const newIdentity = await DWeb.identity.create();
+//   console.log('Created new identity:', newIdentity);
+
+//   const identity = await DWeb.identity.get(newIdentity.did.uri);
+//   console.log('Fetched identity:', identity);
+
+//   const web5Instance = await DWeb.use(identity);
+
+//   const { record } = await web5Instance.dwn.records.create({
+//     data: {
+//       content: 'Hello Web5',
+//       description: 'Keep Building!',
+//     },
+//     message: { published: true, dataFormat: 'application/json' },
+//   });
+
+//   const response = await web5Instance.dwn.records.query({
+//     message: {
+//       filter: {
+//         dataFormat: 'application/json',
+//       },
+//     },
+//   });
+
+//   if (response.status.code === 200) {
+//     response.records.forEach(async (record) => {
+//       console.log(`recordID: ${record._recordId}=> `, await record.data.text());
+//     });
+//   }
+// })();
+
+///NOT WORKING SYNC MOT HAPENING???????
+(async () => {
+  // Initialize the Web5 agent
+  const agent = await DWeb.initialize();
+
+  // Check if an identity exists in storage
+  let storedIdentity = DWeb.storageIdentity.get('identity');
+  let newIdentity;
+  let web5Instance;
+  if (storedIdentity) {
+    console.log('Found stored identity:');
+    console.log('storedIdentity==>', storedIdentity);
+    const oldIdentity = await importIdentity(agent, storedIdentity, true);
+    console.log('oldIdentity', oldIdentity);
+    if (oldIdentity.sync === false) {
+      console.log('Here');
+      web5Instance = await DWeb.use(oldIdentity.exists, { sync: false });
+    } else {
+      web5Instance = await DWeb.use(oldIdentity);
+    }
+  } else {
+    // Create a new identity if none exists in storage
+    newIdentity = await DWeb.identity.create();
+    console.log('Stored Identity NOT FOUND, Created new identity:');
+    // Store the new identity in storage for future use
+    // DWeb.storage.set('identity', { didUri: newIdentity.did.uri });
+    await DWeb.identity.backup(newIdentity);
+    web5Instance = await DWeb.use(newIdentity);
+  }
+
+  console.log('Using identity:');
+
+  // Use the identity with the Web5 instance
+
+  // Create a new record on the Decentralized Web Node (DWN)
+  const { record } = await web5Instance.dwn.records.create({
+    data: {
+      content: 'Hello Web5',
+      description: 'Keep Building!',
+    },
+    message: { published: true, dataFormat: 'application/json' },
+  });
+
+  // Query records from the DWN
+  const response = await web5Instance.dwn.records.query({
+    message: {
+      filter: {
+        dataFormat: 'application/json',
+      },
+    },
+  });
+
+  // If the query was successful, print out the records
+  if (response.status.code === 200) {
+    response.records.forEach(async (record) => {
+      console.log(`recordID: ${record._recordId} =>`, await record.data.text());
+    });
+  }
+})();
